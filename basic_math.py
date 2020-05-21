d356daee9ed2420beacfbe7d727547cb3f619b57
peterpaulolasiman <65737529+peterpaulolasiman@users.noreply.github.com>
2020-05-21 14:25:30 -0700

    Add files via upload

---
+++ basic_math.py
@@ -0,0 +1,52 @@
+'''
+A module that emulates the functionality
+of add, Subtrackt, divide and multiply
+'''
+
+
+def add(x, y):
+    """
+    This function takes in x and y
+    and adds them.
+    :param x: any number
+    :param y: any number
+    :return: the sum of x and y
+    """
+
+    return x + y
+
+def subtract(x, y):
+    """
+    This function takes in x and y
+    and subtract them.
+    :param x: any number
+    :param y: any number
+    :return: the difference of x from  y
+    """
+
+    return x-y
+
+def multiply(x, y):
+    """
+    This function takes in x and y
+    and multiply them.
+    :param x: any number
+    :param y: any number
+    :return: the product of x and y
+    """
+
+    return x * y
+
+def divide(x, y):
+    """
+    This function takes in x and y
+    and divide them.
+    :param x: any number
+    :param y: any number
+    :return: the division of x  by y
+    """
+
+    return x / y
+
+
+
---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+this is the file in which we
+will test the functions that we
+created in basic_math.py
+"""
+import basic_math
+
+if __name__== '__main__':
+    add1 = basic_math.add(10, 100)
+    subtract1= basic_math.subtract(100, 10)
+    multiply1 = basic_math.multiply(10, 10**2)
+    divide1=basic_math.divide(9, 3)
+    print('add1={}, subtract={} multiply1{} divide1={}'.format(add1, subtract1, multiply1, divide1))
